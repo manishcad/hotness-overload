65114a8733c75bf0bce2859934e7e70d53fae737
Manish Chand <manishtochand@gmail.com>
2025-12-18 10:04:19 +0530

    Add files via upload

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-    throw new Error(
-        'Please define the MONGODB_URI environment variable inside .env.local'
-    );
-}
+
 
 interface MongooseCache {
     conn: typeof mongoose | null;
@@ -29,12 +25,18 @@ async function dbConnect() {
         return cached.conn;
     }
 
+    if (!MONGODB_URI) {
+        throw new Error(
+            'Please define the MONGODB_URI environment variable inside .env.local'
+        );
+    }
+
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
         };
 
-        cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         });
     }
